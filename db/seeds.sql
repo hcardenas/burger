e7c924a50525b7e69c60aaa82eb8eeb0f20bbc4d
Helmut Cardenas <aleksey89col@gmail.com>
2017-09-29 20:56:13 -0400

    some

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) values ("Big Mac");
+INSERT INTO burgers (burger_name) values ("Big Daddys Special");
+INSERT INTO burgers (burger_name) values ("Small Mac");
+INSERT INTO burgers (burger_name) values ("Generic Burger");
+INSERT INTO burgers (burger_name) values ("Cheese Burger");
